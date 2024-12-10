d249393530def3a105dd51ca6b8dcc5760a169ea
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2024-12-10 13:59:27 -0400

    corrçaõ

--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,55 @@
 // pages/index.js
 import { useState } from "react";
-import JSZip from "jszip";
-import { saveAs } from "file-saver";
 
 export default function Home() {
-  const [imageSrcs, setImageSrcs] = useState([]);
-  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [imageSrc, setImageSrc] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
 
   const handleFileChange = (event) => {
-    setSelectedFiles(Array.from(event.target.files));
+    setSelectedFile(event.target.files[0]);
   };
 
-  const uploadFiles = async () => {
-    if (selectedFiles.length === 0) {
+  const uploadFile = async () => {
+    if (!selectedFile) {
       console.error("Nenhum arquivo selecionado");
       return;
     }
 
-    const zip = new JSZip();
-    const promises = selectedFiles.map(async (file) => {
-      const formData = new FormData();
-      formData.append("file", file);
+    const formData = new FormData();
+    formData.append("file", selectedFile);
 
-      try {
-        const response = await fetch(
-          "https://borders-converter.squareweb.app/post",
-          {
-            method: "POST",
-            body: formData,
-          }
-        );
-
-        if (response.ok) {
-          const blob = await response.blob();
-          const url = URL.createObjectURL(blob);
-          setImageSrcs((prev) => [...prev, url]);
-          zip.file(file.name, blob);
-        } else {
-          console.error("Erro ao fazer upload do arquivo");
+    try {
+      const response = await fetch(
+        "https://borders-converter.squareweb.app/post",
+        {
+          method: "POST",
+          body: formData,
         }
-      } catch (error) {
-        console.error("Erro ao conectar com a API:", error);
+      );
+
+      if (response.ok) {
+        const blob = await response.blob();
+        const url = URL.createObjectURL(blob);
+        setImageSrc(url);
+      } else {
+        console.error("Erro ao fazer upload do arquivo");
       }
-    });
-
-    await Promise.all(promises);
-
-    if (selectedFiles.length > 1) {
-      zip.generateAsync({ type: "blob" }).then((content) => {
-        saveAs(content, "resultados.zip");
-      });
+    } catch (error) {
+      console.error("Erro ao conectar com a API:", error);
     }
   };
 
   return (
     <div>
       <h1>Upload de Imagem</h1>
-      <input type="file" multiple onChange={handleFileChange} />
-      <button onClick={uploadFiles}>Enviar Imagens</button>
-      {imageSrcs.length > 0 && (
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={uploadFile}>Enviar Imagem</button>
+      {imageSrc && (
         <div>
-          {imageSrcs.map((src, index) => (
-            <div key={index}>
-              <img src={src} alt={`Imagem Processada ${index + 1}`} />
-              <a href={src} download={`resultado${index + 1}.jpg`}>
-                Salvar Imagem
-              </a>
-            </div>
-          ))}
+          <img src={imageSrc} alt="Imagem Processada" />
+          <a href={imageSrc} download="resultado.jpg">
+            Salvar Imagem
+          </a>
         </div>
       )}
     </div>
